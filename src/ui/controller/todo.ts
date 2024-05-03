4d31205dcab355567a8a7496c665665ade8b90a7
andre lima <a.andrelima@outlook.com.br>
2024-05-02 22:36:09 -0300

    nineteen commit

--- src/ui/controller/todo.ts
+++ src/ui/controller/todo.ts
@@ -4,24 +4,7 @@ interface TodoControllerGetParams {
 }
 
 // Vai retornar um dado assincrono, porque eu não sei quando este dado vai retorna.
-async function get({ page }: TodoControllerGetParams = {}) {
-    return fetch("/api/todos").then(async (respostaDoServidor) => {
-        // Pegando os dados que vem do Back-end, ou seja, do banco de dados
-        const todosString = await respostaDoServidor.text();
-        const todosFromServer = JSON.parse(todosString).todos;
-        return todosFromServer;
-
-        // console.log("III", todos);
-        // todos = [...todosFromServer];
-
-        // setTodos(todosFromServer);
-
-        // console.log("YYY", todos);
-
-        // console.log("TODOS", respostaDoServidor);
-        // console.log("TODOS", todos);
-    });
-}
+async function get({ page }: TodoControllerGetParams = {}) {}
 
 export const todoController = {
     get,
--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -1,10 +1,37 @@
-interface TodoRepositoryGetParams {}
+interface TodoRepositoryGetParams {
+    page: number;
+    limit: number;
+}
 
 interface TodoRepositoryGetOutput {
     todos: Todo[];
 }
 
-function get({}: TodoRepositoryGetParams): TodoRepositoryGetOutput {}
+function get({
+    page,
+    limit,
+}: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
+    return fetch("/api/todos").then(async (respostaDoServidor) => {
+        // Pegando os dados que vem do Back-end, ou seja, do banco de dados
+        const todosString = await respostaDoServidor.text();
+        const todosFromServer = JSON.parse(todosString).todos;
+        // return todosFromServer;
+
+        return {
+            todos: todosFromServer,
+        };
+
+        // console.log("III", todos);
+        // todos = [...todosFromServer];
+
+        // setTodos(todosFromServer);
+
+        // console.log("YYY", todos);
+
+        // console.log("TODOS", respostaDoServidor);
+        // console.log("TODOS", todos);
+    });
+}
 
 export const todoRepository = {
     get,
