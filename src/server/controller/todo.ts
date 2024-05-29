0f95c71cf020673e444ca7a93c2ab8618ef5ca45
andre lima <a.andrelima@outlook.com.br>
2024-05-28 21:59:41 -0300

    twenty-nine commit

--- src/server/controller/todo.ts
+++ src/server/controller/todo.ts
@@ -1,11 +1,20 @@
 import { todoRepository } from "@server/repository/todo";
 import { NextApiRequest, NextApiResponse } from "next";
 
-function get(_req: NextApiRequest, res: NextApiResponse) {
-    // Quando for mandar para tela do usuario -> output
-    const output = todoRepository.get();
+function get(req: NextApiRequest, res: NextApiResponse) {
+    const query = req.query;
+
+    console.log("query", query.page);
+
+    // Quando for mandar para tela do usuario --> output
+    const output = todoRepository.get({
+        page: query.page, // P -> 19:50
+        limit: query.limit,
+    });
 
     res.status(200).json({
+        total: output.total,
+        pages: output.pages,
         todos: output.todos,
     });
 }
